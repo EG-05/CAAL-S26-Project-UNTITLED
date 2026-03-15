a0f0fdc1d68f6a17d0d2c4076700baad69f70b42
AyeshaArif-05 <ayesha.arif2705@gmail.com>
2026-03-15 09:13:23 +0500

    Update simple_example.s

--- N-Body-Code/simple_example.s
+++ N-Body-Code/simple_example.s
@@ -7,47 +7,68 @@
 .equ half_dt, 0
 
 # array - 300 floats each
-m:             # masses
+m:             # masses - call address m 300 times with 0.0 float here
     .rept 300 
     .float 0 
     .endr
 p_x:           # position
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 p_y:                  
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 p_z:                  
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 v_x:            # velocity
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 v_y:                  
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 v_z:                  
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 a_x:           # acceleration
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 a_y:                  
     .rept 300 
-    .float 0 
+    .float 0
     .endr
 a_z:                  
     .rept 300 
-    .float 0 
+    .float 0
     .endr
+
 .section .text
 .globl main
+
+main:
+    # main loop will go here
+    call kick
+    call drift
+    call calculate_acc
+    call kick
+
+kick:
+    la t0, v_x
+    la t1, v_y
+    ret
+
+drift:
+    # drift function goes here
+    ret
+
+calculate_acc:
+    # calculate_acceleration goes here
+    ret
 
 
